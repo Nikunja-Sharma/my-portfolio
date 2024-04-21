518d8361e945b95de01686a3bc722ee72146a09a
Nikunja-Sharma <xnikunja@gmail.com>
2024-04-21 23:23:04 +0530

    social links in middleware

--- app/(social)/github/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-import React from "react";
-
-const page = () => {
-    redirect("https://github.com/Nikunja-Sharma");
-};
-
-export default page;
---
+++ middleware.ts
@@ -0,0 +1,60 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: Request) {
+    if (request.url === `${process.env.PUBLIC_URL}/x`) {
+        return NextResponse.redirect(
+            new URL(
+                "https://twitter.com/nikunja__",
+                request.url
+            )
+        );
+    }
+    if (request.url === `${process.env.PUBLIC_URL}/reddit`) {
+        return NextResponse.redirect(
+            new URL(
+                "https://www.reddit.com/user/NikunjaSarma",
+                request.url
+            )
+        );
+    }
+    if (request.url === `${process.env.PUBLIC_URL}/discord`) {
+        return NextResponse.redirect(
+            new URL(
+                "https://discordapp.com/users/396122165734539265",
+                request.url
+            )
+        );
+    }
+    if (request.url === `${process.env.PUBLIC_URL}/github`) {
+        return NextResponse.redirect(
+            new URL(
+                "https://github.com/Nikunja-Sharma",
+                request.url
+            )
+        );
+    }
+    if (request.url === `${process.env.PUBLIC_URL}/instagram`) {
+        return NextResponse.redirect(
+            new URL(
+                "https://instagram.com/nikunja___",
+                request.url
+            )
+        );
+    }
+    if (request.url === `${process.env.PUBLIC_URL}/linkedin`) {
+        return NextResponse.redirect(
+            new URL(
+                "https://www.linkedin.com/in/nikunja-sarma",
+                request.url
+            )
+        );
+    }
+    if (request.url === `${process.env.PUBLIC_URL}/twitch`) {
+        return NextResponse.redirect(
+            new URL(
+                "https://www.twitch.tv/xnikunja",
+                request.url
+            )
+        );
+    }
+}
