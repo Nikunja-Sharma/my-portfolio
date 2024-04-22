ca691273f57fc510c7b65b829c2799569d27e002
Nikunja-Sharma <xnikunja@gmail.com>
2024-04-22 14:44:00 +0530

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,44 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/x',
+                destination: 'https://twitter.com/nikunja__',
+                permanent: false,
+              },
+              {
+                source: '/reddit',
+                destination: 'https://www.reddit.com/user/NikunjaSarma',
+                permanent: false,
+              },
+              {
+                source: '/discord',
+                destination: 'https://discordapp.com/users/396122165734539265',
+                permanent: false,
+              },
+              {
+                source: '/github',
+                destination: 'https://github.com/Nikunja-Sharma',
+                permanent: false,
+              },
+              {
+                source: '/instagram',
+                destination: 'https://instagram.com/nikunja___',
+                permanent: false,
+              },
+              {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/nikunja-sarma',
+                permanent: false,
+              },
+              {
+                source: '/twitch',
+                destination: 'https://www.twitch.tv/xnikunja',
+                permanent: false,
+              },
+        ];
+      },
+};
 
 export default nextConfig;
